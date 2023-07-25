a89cb70b73d5a9548987c98b0462c19057da30c3
aryanch12 <114851758+aryanch12@users.noreply.github.com>
2023-07-25 18:05:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -590,3 +590,645 @@
 
 // Because of this, the sort() method will produce an incorrect 
 // result when sorting numbers.
+
+
+
+// result when sorting numbers.
+
+
+
+//                                                                       Section 10👉 Document Object model in JavaScript 
+
+// 1️⃣ Window is the main container or we can say the 
+// global Object and any operations related to entire 
+// browser window can be a part of window object. 
+
+
+// For ex 👉 the history or to find the url etc.
+
+// 1️⃣ whereas the DOM is the child of Window Object
+
+// 2️⃣ All the members like objects, methods or properties. 
+// If they are the part of window object then we do not refer 
+// the window object. Since window is the global object 
+// so you do not have to write down window. 
+// - it will be figured out by the runtime.
+
+
+// For example
+// 👉 window.screen or just screen is a small information 
+// object about physical screen dimensions.
+// 👉 window.location giving the current URL
+// 👉 window.document or just document is the main object 
+// of the potentially visible (or better yet: rendered) 
+// document object model/DOM.
+
+// 2️⃣ Where in the DOM we need to refer the document, 
+// if we want to use the document object, methods or properties
+// For example
+// 👉 document.getElementById()
+
+
+// 3️⃣ Window has methods, properties and object. 
+// ex setTimeout() or setInterval() are the methods 
+// where as Document is the object of the Window and 
+// It also has a screen object with properties 
+// describing the physical display.
+
+
+// Now, I know you have a doubt like we have seen the methods 
+// and object of the global object that is window. But What about
+// the properties of the Window Object 🤔
+
+// so example of window object properties are
+// innerHeight, 
+// innerWidth and there are many more 
+
+// let's see some practical in DOM HTML file
+
+
+
+
+
+
+
+
+
+
+// ************** DOM vs BOM *******************
+
+// 👉 The DOM is the Document Object Model, which deals with the document, 
+// the HTML elements themselves, e.g. document and all traversal you 
+// would do in it, events, etc.
+
+// For Ex: 👨‍🏫
+// change the background color to red
+// document.body.style.background = "red";
+
+
+// 👉 The BOM is the Browser Object Model, which deals with browser components 
+// aside from the document, like history, location, navigator and screen 
+// (as well as some others that vary by browser). OR 
+// In simple meaning all the Window operations which comes under BOM are performed 
+// usign BOM 
+
+
+
+// Let's see more practical on History object 
+
+// Functions alert/confirm/prompt are also a part of BOM: 
+// they are directly not related to the document, 
+// but represent pure browser methods of communicating with the user.
+
+// alert(location.href); // shows current URL
+// if (confirm("Want to Visit ThapaTechnical?")) {
+//   location.href = "https://www.youtube.com/thapatechnical"; // redirect the browser to another URL
+// }
+
+// Section 3️⃣: Navigate through the DOM 
+
+// 1: document.documentElement
+      // returns the Element that is the root element of the document. 
+// 2: document.head
+// 3: document.body
+// 4: document.body.childNodes (include tab,enter and whiteSpace)
+  // list of the direct children only 
+// 5: document.children (without text nodes, only regular Elements)
+// 6: document.childNodes.length
+
+
+// 👉 Practice Time 
+// How to find the child in DOM tree 
+// firstChild vs firstElementChild
+// lastChild vs lastElementChild 
+// const data = document.body.firstElementChild;
+// undefined
+// data
+// data.firstElementChild
+// data.firstElementChild.firstElementChild
+// data.firstElementChild.firstElementChild.style.color = "red"
+// vs 
+// document.querySelector(".child-two").style.color = "yellow";
+
+// 👉 How to find the Parent Nodes 
+// document.body.parentNode
+// document.body.parentElement
+
+// 👉 How to find or access the siblings
+// document.body.nextSibling
+// document.body.nextElementSibling
+// document.body.previousSibling
+// document.body.previousElementSibling
+
+
+
+
+
+
+
+
+                                                              /**** Section 11👉 EVENTS in JavaScript ****/
+// HTML events are "things" that happen to HTML elements.
+// When JavaScript is used in HTML pages, JavaScript can "react" on these events.
+
+
+// 👨‍🏫 HTML Events
+// An HTML event can be something the browser does, or something a user does.
+
+
+// Here are some examples of HTML events:
+
+// An HTML web page has finished loading
+// An HTML input field was changed
+// An HTML button was clicked
+// Often, when events happen, you may want to do something.
+
+
+// section 1️⃣ 4 ways of writing Events in JavaScript
+
+// 1: using inline events alert();
+// 2: By Calling a funcion (We already seen and most common way of writing) 
+// 3: using Inline events (HTML onclick="" property and element.onclick)
+// 4: using Event Listeners (addEventListener and IE's attachEvent)
+
+// check the Events HTML File
+
+
+
+// section 2️⃣: What is Event Object?
+// Event object is the parent object of the event object. 
+// for Example 
+// MouseEvent, focusEvent, KeyboardEvent etc
+
+// section ️3️⃣ MouseEvent in JavaScript
+// The MouseEvent Object
+// Events that occur when the mouse interacts with the HTML 
+// document belongs to the MouseEvent Object.
+
+//                                                           formm showing or not
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+// </head>
+// <body>
+//     <button onclick="toggleForm()">Show Form</button>
+//     <form id="demo" style="display: none;">
+//         <label for="fname">First name:</label><br>
+//         <input type="text" id="fname" name="fname"><br>
+//         <label for="lname">Last name:</label><br>
+//         <input type="text" id="lname" name="lname">
+//     </form>
+
+//     <script>
+//         function toggleForm() {
+//             var form = document.getElementById("demo");
+//             if (form.style.display === "none") {
+//                 form.style.display = "block";
+//             } else {
+//                 form.style.display = "none";
+//             }
+//         }
+//     </script>
+// </body>
+// </html>
+
+//                                                  The ondblclick Event
+
+
+/* <p ondblclick="myFunction()">Double-click this paragraph to trigger a function.</p>
+
+<p id="demo"></p>
+
+<script>
+function myFunction() {
+  document.getElementById("demo").innerHTML += "Hello World ";
+}
+</script> */
+
+                                                //The onmousedown Event
+
+/* <p>Clock the text below!</p>
+<p id="myP" onmousedown="mouseDown()" onmouseup="mouseUp()">
+The mouseDown() function sets the color of this text to red.
+The mouseUp() function sets the color of this text to blue.
+</p>
+
+<script>
+function mouseDown() {
+  document.getElementById("myP").style.color = "red";
+}
+
+function mouseUp() {
+  document.getElementById("myP").style.color = "blue";
+}
+</script> */
+
+
+
+
+                                                        //The onmouseenter Event
+
+
+/* <img onmouseenter="bigImg(this)" onmouseleave="normalImg(this)" border="0" src="smiley.gif" alt="Smiley" width="32" height="32">
+
+<p>The function bigImg() is triggered when the user moves the mouse pointer onto the image.</p>
+<p>The function normalImg() is triggered when the mouse pointer is moved out of the image.</p>
+
+<script>
+function bigImg(x) {
+  x.style.height = "64px";
+  x.style.width = "64px";
+}
+
+function normalImg(x) {
+  x.style.height = "32px";
+  x.style.width = "32px";
+} */
+
+
+
+
+
+
+// 👉 object oriented Javascript
+
+// **********************************************************************
+
+
+// 1️⃣ What is Object Literal?
+
+// Object literal is simply a key:value pair data structure. 
+
+// Storing variables and functions together in one container, 
+// we can refer this as an Objects.
+
+
+
+ 
+// let biodata={
+//   myName:"aryan",
+//   myAge:26,
+//   getData:function(){
+//     console.log(`My name is ${biodata.myName} and my age is ${biodata.myAge}`);
+//   }
+// }
+// biodata.getData();
+
+// 2nd way no need to write functions as well after es6
+
+// let biodata={
+//   myName:"aryan chaudhary",
+//   myAge:26,
+//   getData(){
+//     console.log(`my name is ${biodata.myName} and my age is ${biodata.myAge}`);
+//   }
+
+// }
+// biodata.getData();
+
+// function myName(){
+//   console.log(this);
+// }
+// myName();
+
+
+// var myNames="aryan";
+// function myName(){
+//   console.log(this.myNames);
+// }
+// myName();
+
+
+// // ex 3 
+
+// var myNames = 'vinod';
+// function myName() {
+//     console.log(this.myNames);
+// }
+// myName();
+
+
+// // ex 4 
+// const obj = {
+//     myAge : 26,
+//     myName() {
+//       console.log(this.myAge);
+//     }
+// }
+// obj.myName();
+
+// // ex 5 
+// // this object will not work with arrow function bcz arrow function is bound to class.
+
+// const obj = {
+//     myAge : 26,
+//     myName : () => {
+//       console.log(this);
+//     }
+// }
+// obj.myName();
+
+
+
+// // ex 6
+
+// let bioData = {
+//     myName : {
+//         realName : "vinod thapa",
+//         channelName : 'thapa technical'
+//     },
+//     // things to remember is that the myName is the key and the object is act like a value 
+//     myAge : 26,
+//     getData (){
+//       console.log(`My name is ${this.myName.channelName} and my age is ${this.myAge} `);
+//     }
+//   }
+
+//   bioData.getData();
+
+
+// // call method is used to call the method of another object 
+// // or with call(), an object can use a method belonging to another object 
+
+// // But as per other it is simply the way to use the this keyword or another object 
+
+
+
+
+
+
+
+
+
+
+// // **********************************************************************
+
+// // 👉 How JavaScript Works? Advanced and Asynchronous JavaScript
+
+// // **********************************************************************
+
+
+// //                                                     Advanced JavaScript Section
+
+// // 1️⃣: Event Propagation (Event Bubbling and Event Capturing)
+
+// // check html file 
+
+// // 2️⃣: Higher Order Function
+// // function which takes another function as an arguments is called HOF 
+// // wo function jo dusre function ko as an argument accept krta hai use HOF 
+
+// callback function
+// // function which get passed as an argument to another function is called CBF 
+// // A callback function is a function that is passed as an argument to 
+// // another function, to be “called back” at a later time. 
+
+// // Jis bhi function ko hum kisi or function ke under as an arguments passed 
+// // krte hai then usko hum CallBack fun bolte hai
+
+// // // we need to create a calculator 
+// const add=(a,b)=>{
+//   return a+b;
+// }
+// console.log(add(5,2));
+
+// const subs =(a,b)=>{
+//   return Maths.abs(a-b);
+// }
+// const mult=(a,b)=>{
+//   return a*b;
+// }
+
+// const calculator=(num1,num2,operator)=>{
+//   return operator(num1,num2);
+// }
+
+// calculator(5,2,subs)
+// console.log(calculator(5,2,subs));
+
+// // // I have to do the hardcoded for each operation which is bad
+// // // we will use the callback and the HOF to make it simple to use 
+
+// // // Now instead of calling each function indivisually we can call it
+// // // by simply using one function that is calculator 
+
+// console.log(calculator(5,6,add));
+// console.log(calculator(5,6,subs));
+// console.log(calculator(5,6,mult));
+
+// // In the above example, calculator is the higher-order function, 
+// // which accepts three arguments, the third one being the callback.
+// // Here the calculator is called the Higher Order Function because it takes
+// // another function as an argument 
+
+// // and add, sub and mult are called the callback function bcz they are passed 
+// // as an argument to another fucntion 
+
+// // InterView Question 
+// // Difference Between Higher Order Function and Callback Function ?
+
+// // 🏁🏁Asynchronous JavaScript
+
+
+// // 6️⃣:   Synchronous JavaScript Prog 
+
+// const fun2=()=>{
+//   console.log(`function 2 is called`);
+// }
+
+// const fun1=()=>{
+//   console.log(`function 1 is called`);
+// fun2();
+// console.log(`function 1  is called again.`);
+// }
+// fun1();
+
+
+
+
+
+// const fun2=()=>{
+//   setTimeout(()=>{
+//   console.log(`function 2 is called`)
+// },2000);
+// }
+
+// const fun1=()=>{
+//   console.log(`function 1 is called`);
+// fun2();
+// console.log(`function 1  is called again.`);
+// }
+// fun1();
+
+// //👻 What is Event Loop in JavaScript?
+// // ppt explain 
+
+
+// // 5️⃣ Hoisting in JavaScript 
+
+// // we have a creation phase and execution phase.
+
+// // Hoisting in Javascript is a mechanism where variables and functions
+// // declarations are moved to the top of their scope before the code execute.
+
+//For example 
+// console.log(myName);
+// let myName;
+// myName = "thapa";
+
+// // How it will be in output during creation phase 
+
+// var myName=undefined;
+// console.log(myName);
+// myName="aryan";
+
+
+
+// / //😲 In ES2015 (a.k.a. ES6), hoisting is avoided by using the let keyword 
+// // // instead of var. (The other difference is that variables declared 
+// // // with let are local to the surrounding block, not the entire function.)
+
+
+
+
+// // 6️⃣ What is Scope Chain and Lexical Scoping in JavaScript? 
+
+// // The scope chain is used to resolve the value of variable names 
+// // in JS.
+
+// // scope chain in js is lexically defined, which means that we can 
+// // see what the scope chain will be by looking at the code. 
+
+// // At the top, we have the Global Scope, which is the window Object
+// // in the browser.
+
+// // Lexical Scoping means Now, the inner function can get access to 
+// // their parent functions variables But the vice-versa is not true.
+
+
+//For Example
+
+// let a="hello guys";//global scope
+
+// const first=()=>{
+//   let b="how are you?"
+
+//   const second=()=>{
+//     let c="hi, i am fine thanku";
+//     console.log(a+b+c);
+//   }
+//   second();
+//   // console.log(a+b+c); //I can't use C 
+// }
+// first();
+
+
+// // 7️⃣ What is Closures in JavaScript 🤔
+
+// // A closure is the combination of a function bundled together (enclosed) with references 
+// // to its surrounding state (the lexical environment). 
+
+// // In other words, a closure gives you 
+// // access to an outer function’s scope from an inner function. 
+
+// // In JavaScript, closures are created every time a function is created, at function creation time.
+
+// // For Example 👇
+
+// const outerFun = (a) => {
+//     let b = 10;
+//     const innerFun = () => {
+//       let sum = a+b;
+//       console.log(`the sum of the two no is ${sum}`);
+//     }
+//     innerFun();
+// }
+// outerFun(5);
+
+
+// // it same like lexical scoping 
+
+// // One more Example 👇
+
+// const outerFun = (a) => {
+//   let b = 10;
+//   const innerFun = () => {
+//     let sum = a+b;
+//     console.log(`the sum of the two no is ${sum}`);
+//   }
+//   return innerFun;
+// }
+// let checkClousure = outerFun(5);
+// console.dir(checkClousure);
+
+
+// Currying
+
+// const sum=(num1)=>(num2)=>(num3)=> console.log(num1+num2+num3);
+// sum(5)(3)(6);
+
+// // 8️⃣: CallBack Hell 
+// setTimeout(()=>{
+//   console.log(`1works is done`);
+//   setTimeout(()=>{
+//     console.log(`2 work is done`);
+//     setTimeout(()=>{
+//       console.log(`3works is done`);
+//       setTimeout(()=>{
+//         console.log(`4 work is done`);
+//       },1000)
+//     },1000)
+//   },1000)
+// },1000)
+
+
+
+
+// // **********************************************************************
+
+// // 👉 // 🤩 Bonus JSON 🤩
+
+// // **********************************************************************
+
+// // 👉 JSON.stringify turns a JavaScript object into JSON text and 
+// stores that JSON text in a string, eg:
+// var my_object={key_1:"some text",key_2:true,key_3:5};
+
+// var object_as_string=JSON.stringify(my_object);
+// // "{"key_1":"some text","key_2":true,"key_3":5}"
+// console.log(object_as_string);
+// typeof(object_as_string);
+
+
+
+
+
+
+
+// // 7️⃣ AJAX Call using XMLHttprequest 
+
+// // how to handled with the events and callback 
+
+// // XMLHttpRequest (XHR) objects are used to interact with servers. 
+// // You can retrieve data from a URL without having to do a full 
+// // page refresh. This enables a Web page to update just part 
+// // of a page without disrupting what the user is doing. 
+// // XMLHttpRequest is used heavily in AJAX programming.
+
+// const request = new XMLHttpRequest();
+// // we need to call the api or request the api using GET method ki, me jo
+// // url pass kar kr rha hu uska data chaiye 
+// request.open('GET', "https://covid-api.mmediagroup.fr/v1");
+// request.send(); // we need to send the request and its async so we need to 
+// // add the event to load the data adn get it
+
+// // to get the response 
+// request.addEventListener("load", () => {
+//     console.log(this.responseText);
+// });
+
